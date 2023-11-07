8c4e0eaf0c3a0b5034e7517ead0956af0e27fc3e
elie2312 <elie.bannes12@gmail.com>
2023-11-07 15:39:58 +0100

    modif ex7

--- ex7.sql
+++ ex7.sql
@@ -1,10 +1,4 @@
 -- Requête 7. Lister les entités et leurs pays d'origine :
 
-SELECT e.name AS entité, c.country AS pays_origine
-FROM entity AS e
-LEFT JOIN country AS c ON e.id = c.id;
-
-
-
-
-
+SELECT e.name, a.countries
+FROM entity e JOIN address a ON e.id_address = a.id_address;
